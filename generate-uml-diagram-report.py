002be41d589a31bf2e6d39aaff02c469ea1c36dd
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-06-20 13:37:32 +0300

    Create generate-uml-diagram-report.py

---
+++ generate-uml-diagram-report.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import plantuml
+
+def generate_urls(directory, output_file):
+    # Getting PlantUML server URL from environment variable or using default value
+    puml_server_url = os.getenv('PUML_SERVER_URL', 'https://www.plantuml.com/plantuml/svg/')
+    pl = plantuml.PlantUML(url=puml_server_url)
+
+    markdown_content = "# Diagrams\n\n<details><summary>Diagrams summary</summary>\n\n"
+    markdown_content += "|**URL**|**PATH**|\n|:---|:---|\n"
+
+    # Search all .puml files in a directory and subdirectories
+    for file_path in glob.glob(os.path.join(directory, '**/*.puml'), recursive=True):
+        file_name, _ = os.path.splitext(os.path.basename(file_path))
+        with open(file_path, 'r') as file:
+            diagram = file.read()
+
+        url = pl.get_url(diagram)
+        markdown_content += f"|[{file_name} URL]({url})|{file_path}|\n"
+        
+    markdown_content += "\n</details>\n"
+
+    # Write all links to the output file
+    with open(output_file, 'w') as f:
+        f.write(markdown_content)
+
+if __name__ == "__main__":
+    # Getting directory path from environment variable or using default value
+    directory = os.getenv('FILE_PATH', '.')
+    output_file = 'uml-diagram-report'
+    generate_urls(directory, output_file)
